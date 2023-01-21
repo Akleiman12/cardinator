bed21306958a9280b69e585f0f6ecca74c71a37a
Asher Kleiman <asherkleiman@xpertai.com>
2023-01-21 02:30:12 +0100

    functional data-keeper service

--- src/data-keeper/data-keeper.module.ts
+++ src/data-keeper/data-keeper.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { DataKeeperService } from './data-keeper.service';
 
 @Module({
-  providers: [DataKeeperService]
+  providers: [DataKeeperService],
+  exports: [DataKeeperService]
 })
 export class DataKeeperModule {}
--- src/data-keeper/data-keeper.service.ts
+++ src/data-keeper/data-keeper.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 
 import * as fs from 'fs';
 
-export enum DataTypes {
+export enum DataTypesEnum {
     User = 'USER',
     Card = 'CARD',
     Offer = 'OFFER',
@@ -11,23 +11,30 @@ export enum DataTypes {
 @Injectable()
 export class DataKeeperService {
 
+    /**
+     * @description Save Data of 'type' in local file system 
+     */
     async setData(type: string, data: unknown) {
         const dataString = JSON.stringify(data);
-        fs.writeFile(`tmp/${type}`, dataString, (err) => {
+        console.log(`Setting ${type} => ${dataString}`);
+        fs.writeFile(`./data/${type}`, dataString, (err) => {
             if (err) {
                 console.log(err);
-                throw new Error(`Error setting: ${type} => ${dataString}`);
+                throw new InternalServerErrorException(`Error setting: ${type} => ${dataString}`);
             }
         })
     }
 
+    /**
+     * @description Recover Data of 'type' from local file system 
+     */
     getData(type: string) {
         try {
-            const dataBuffer = fs.readFileSync(`tmp/${type}`);
-            return JSON.stringify(dataBuffer.toString());
+            const dataBuffer = fs.readFileSync(`./data/${type}`);
+            return JSON.parse(dataBuffer.toString());
         } catch (err) {
             console.log(err);
-            throw new Error(`Error getting: ${type}`);
+            throw new InternalServerErrorException(`Error getting: ${type}`);
         }
     }
 }
