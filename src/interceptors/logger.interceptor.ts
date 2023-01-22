56fb09e75b1fc02ceac7facae4638ec2f1c63ef3
Asher Kleiman <asherkleiman@xpertai.com>
2023-01-22 01:42:43 +0100

    added logger interceptor

---
+++ src/interceptors/logger.interceptor.ts
@@ -0,0 +1,19 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class LoggerInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const req = context.switchToHttp().getRequest();
+    const user = req.user;
+    const body = req.body;
+    console.log('REQUEST => ', { timestamp: new Date(), user, body })
+    return next.handle().pipe(
+      map(
+        (response) => {
+          console.log('RESPONSE => ', { timestamp: new Date(), response })
+        }
+      )
+    );
+  }
+}
